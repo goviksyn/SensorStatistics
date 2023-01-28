f9924aa98196f1b75da9ce7ba3948ff9f4817854
govind <42165200+goviksyn@users.noreply.github.com>
2023-01-28 13:56:50 +0800

    Update FileDataProcessor.scala

--- src/main/scala/processor/FileDataProcessor.scala
+++ src/main/scala/processor/FileDataProcessor.scala
@@ -10,12 +10,31 @@ trait FileDataProcessor {
 
   def run(directoryName: String): IO[ResultCollector]
 
+  /**
+   * The getInputFiles method takes in a directory name and returns a sequence of files that are present
+   * in that directory and have the ".csv" extension.
+   * It first checks if the directory is valid, throws an exception if it is not.
+   * It then filters the files in the directory by csv extensions and returns the filtered files as a sequence.
+   * @param directoryName
+   * @return
+   */
   def getInputFiles(directoryName: String): Seq[File] = {
     val inputDirectory = new File(directoryName)
     if (!inputDirectory.isDirectory) throw new IllegalArgumentException("Invalid directory")
     inputDirectory.listFiles((_, name) => name.endsWith(".csv")).toIndexedSeq
   }
 
+  /**
+   * The processDataFile method that takes in a file and an object that collects the results of processing the file.
+   * It uses the bracket method to safely acquire and release a resource,
+   * in this case, the CSV file. Inside the bracket, it reads the file, extracts the required data, and adds it to the
+   * outputData object.
+   * It increments the fileCount field of the outputData object. Finally, the method returns the outputData object,
+   * which is wrapped in an IO effect.
+   * @param file
+   * @param outputData
+   * @return
+   */
   protected def processDataFile(file: File, outputData: ResultCollector = ResultCollector()): IO[ResultCollector] = {
     val csvIO = IO(CSVReader.open(file))
 
